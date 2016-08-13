da0ac86a3b0740a2259d8fbed30e373206d93434
Ujjal Suttra Dhar <usuttra@yahoo.com>
2016-08-13 16:18:20 +0600

    Updated base_url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/works2015/manageVC';
+$config['base_url']	= 'http://site.domain';
 
 /*
 |--------------------------------------------------------------------------
